d9e7ef716aed51c660c2b5f860c3e8181d3855a6
mhanauer <mhanauer@indiana.edu>
2020-09-13 10:57:46 -0400

    Add files via upload

---
+++ first_py_test.py
@@ -0,0 +1,8 @@
+# Run in ana command prompt streamlit run first_py_test.py
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+st.title('My first app')
+
