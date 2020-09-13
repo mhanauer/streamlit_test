bc7fedc81135a2aa88532dce47b38b77a69124c3
mhanauer <mhanauer@indiana.edu>
2020-09-13 11:11:09 -0400

    Delete first_py_test.py

--- first_py_test.py
+++
@@ -1,8 +0,0 @@
-# Run in ana command prompt streamlit run first_py_test.py
-import streamlit as st
-# To make things easier later, we're also importing numpy and pandas for
-# working with sample data.
-import numpy as np
-import pandas as pd
-st.title('My first app')
-
